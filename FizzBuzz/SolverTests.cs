32884ef047d5eef3a84590ccae194a7b5a9a8e93
Marco Rasp <marco.rasp@googlemail.com>
2016-01-22 11:26:42 +0100

    first simple test

---
+++ FizzBuzz/SolverTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class SolverTests
+    {
+        [Test]
+        public void When_Translate_is_called_with_1_then_result_should_be_1()
+        {
+            var solver = Solver();
+            var result = solver.Translate(1);
+            Assert.AreEqual("1", result);
+        }
+    }
+}
