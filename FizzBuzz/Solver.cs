469a8e748ec537234eaaf2b9ee5855588c7c9541
Marco Rasp <marco.rasp@googlemail.com>
2016-01-22 12:17:03 +0100

    first fail implementation

---
+++ FizzBuzz/Solver.cs
@@ -0,0 +1,14 @@
+﻿namespace FizzBuzz
+{
+    internal class Solver
+    {
+        public Solver()
+        {
+        }
+
+        public object Translate(int i)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
