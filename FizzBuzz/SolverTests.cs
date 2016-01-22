588ab72abec540290e6c8a275c037ae1fc84c3de
Marco Rasp <marco.rasp@googlemail.com>
2016-01-22 13:24:58 +0100

    next simple fail test

--- FizzBuzz/SolverTests.cs
+++ FizzBuzz/SolverTests.cs
@@ -12,5 +12,13 @@ namespace FizzBuzz
             var result = solver.Translate(1);
             Assert.AreEqual("1", result);
         }
+
+        [Test]
+        public void When_Translate_is_called_with_2_then_result_should_be_2()
+        {
+            var solver = new Solver();
+            var result = solver.Translate(1);
+            Assert.AreEqual("2", result);
+        }
     }
 }
