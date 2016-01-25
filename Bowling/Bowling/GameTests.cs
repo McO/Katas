4869df18f30a3b5a610b953967721c7bf29e3f65
Marco Rasp <marco.rasp@googlemail.com>
2016-01-25 13:45:00 +0100

    finished tests with no score

--- Bowling/Bowling/GameTests.cs
+++ Bowling/Bowling/GameTests.cs
@@ -9,10 +9,25 @@ namespace Bowling
         public void LoserGame()
         {
             var game = new Game();
+            for (var i = 1; i <= 21; i++)
+                game.Roll(0);
+
+            Assert.AreEqual(0, game.Score());
         }
     }
 
     public class Game
     {
+        private int _score;
+
+        public void Roll(int pins)
+        {
+            _score += pins;
+        }
+
+        public int Score()
+        {
+            return _score;
+        }
     }
 }
