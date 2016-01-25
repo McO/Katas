6caa12eba43cdfc644fe027edc7cca0e46c8233b
Marco Rasp <marco.rasp@googlemail.com>
2016-01-25 14:05:07 +0100

    test with 20x 1

--- Bowling/Bowling/GameTests.cs
+++ Bowling/Bowling/GameTests.cs
@@ -6,13 +6,23 @@ namespace Bowling
     public class GameTests
     {
         [Test]
-        public void LoserGame()
+        public void Game_with_no_score()
         {
             var game = new Game();
             for (var i = 1; i <= 21; i++)
                 game.Roll(0);
 
             Assert.AreEqual(0, game.Score());
+        }
+
+        [Test]
+        public void Game_with_all_Ones()
+        {
+            var game = new Game();
+            for (var i = 1; i <= 20; i++)
+                game.Roll(1);
+
+            Assert.AreEqual(20, game.Score());
         }
     }
 
