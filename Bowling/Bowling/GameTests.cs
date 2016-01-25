1901b03a188e5767fbc58d21891209b766743d35
Marco Rasp <marco.rasp@googlemail.com>
2016-01-25 13:41:23 +0100

    initial checkin with first simple test

---
+++ Bowling/Bowling/GameTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Bowling
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void LoserGame()
+        {
+            var game = new Game();
+        }
+    }
+
+    public class Game
+    {
+    }
+}
